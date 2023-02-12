e17556dc942132c1b357864b825a893bbf3017c7
UO276723 <uo276723@uniovi.es>
2023-02-12 15:26:21 +0100

    SDI-2223-804-2.1-Create Spring project.

---
+++ src/main/java/com/uniovi/sdi2223804spring/Sdi2223804SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2223804spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2223804SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2223804SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniovi/sdi2223804spring/Sdi2223804SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2223804spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2223804SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
