30118843b11298033daa5610783939b8a029d477
UO276723 <uo276723@uniovi.es>
2023-02-12 19:13:00 +0100

    SDI-2223-804-2.4-Modelo, acceso a datos simple.

---
+++ src/main/java/com/uniovi/sdi2223804spring/repositories/MarksRepository.java
@@ -0,0 +1,6 @@
+package com.uniovi.sdi2223804spring.repositories;
+import com.uniovi.sdi2223804spring.entities.Mark;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MarksRepository extends CrudRepository<Mark, Long> {
+}
