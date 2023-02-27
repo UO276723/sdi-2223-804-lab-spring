9cf1c3aa360cfca5d7b0f5256fa3313ed25265ed
uo276723 <uo276723@uniovi.es>
2023-02-27 12:44:39 +0100

    SDI-2223-804-sesión4-Ejercicio-complementario2

--- src/main/java/com/uniovi/sdi2223804spring/repositories/MarksRepository.java
+++ src/main/java/com/uniovi/sdi2223804spring/repositories/MarksRepository.java
@@ -1,6 +1,11 @@
 package com.uniovi.sdi2223804spring.repositories;
 import com.uniovi.sdi2223804spring.entities.Mark;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.awt.print.Pageable;
 
 public interface MarksRepository extends CrudRepository<Mark, Long> {
 }
+
