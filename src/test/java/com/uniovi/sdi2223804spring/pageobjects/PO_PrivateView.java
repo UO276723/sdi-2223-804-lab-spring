1669077aada13dafdc1a55ee745c9440874cb028
uo276723 <uo276723@uniovi.es>
2023-03-10 10:59:37 +0100

    SDI-2223-804-sesión5-ejercicio-complementario1

--- src/test/java/com/uniovi/sdi2223804spring/pageobjects/PO_PrivateView.java
+++ src/test/java/com/uniovi/sdi2223804spring/pageobjects/PO_PrivateView.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
 public class PO_PrivateView extends PO_NavView {
 
 
@@ -23,5 +26,25 @@ public class PO_PrivateView extends PO_NavView {
         score.sendKeys(scorep);
         By boton = By.className("btn");
         driver.findElement(boton).click();
+    }
+
+    static public void login(WebDriver driver, String userNamed, String passwordd){
+        //Vamos al formulario de logueo.
+        PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+        //Rellenamos el formulario
+        PO_LoginView.fillLoginForm(driver, userNamed, passwordd);
+        String checkText = "Notas del usuario";
+        List<WebElement> result = PO_View.checkElementBy(driver, "text", checkText);
+        PO_View.checkElementBy(driver, "text", userNamed);
+    }
+
+    static public void logout(WebDriver driver){
+        String loginText = PO_HomeView.getP().getString("signup.message", PO_Properties.getSPANISH());
+        PO_PrivateView.clickOption(driver, "logout", "text", loginText);
+    }
+
+    static public void clickOnElement(WebDriver driver, String textd, int elementd){
+        List<WebElement> elements = PO_View.checkElementBy(driver, "free", textd);
+        elements.get(elementd).click();
     }
 }
